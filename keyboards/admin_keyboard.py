3cb384b297ee77381d27e709e14a018ade185e3c
Artem Shevlyakov <99600964+temashev@users.noreply.github.com>
2022-08-03 23:08:36 +0300

    Delete admin_keyboard.py

--- keyboards/admin_keyboard.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton  # , ReplyKeyboardRemove
-
-
-cens_on = KeyboardButton('/cens_on')
-cens_off = KeyboardButton('/cens_off')
-
-kb_cens = ReplyKeyboardMarkup(resize_keyboard=True)
-
-kb_cens.row(cens_on, cens_off)
